e51823b5c17764a54b8e8da618366ec2a2333cd6
Hasan <mr.k779@outlook.com>
2021-11-14 14:55:40 +0600

    Setting Up Bengali

--- calc.py
+++ calc.py
@@ -1,21 +1,3 @@
-import os
-from pathlib import Path
+from util.text_transform import TextTransform
 
-_ext_txt = ".trans.txt"
-_ext_audio = ".flac"
-
-
-def add(x, y):
-    return x + y
-
-
-def subtract(x, y):
-    return x - y
-
-
-# Get string representation of 'root' in case Path object is passed
-root = os.fspath("./asr_bengali")
-
-walker = sorted(str(p.stem) for p in Path(root).glob('*/*/*' + _ext_audio))
-print(len(walker))
-print(walker[0])
+t = TextTransform()
