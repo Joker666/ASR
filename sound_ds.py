ce4937866603440e146dc8f4066b3d138a3032c5
Hasan <mr.k779@outlook.com>
2021-11-20 18:31:31 +0600

    Updated sound ds

--- sound_ds.py
+++ sound_ds.py
@@ -1,12 +1,38 @@
-import os, zipfile
-from typing import Tuple, Union
+import os
+import zipfile
 from pathlib import Path
-import pandas as pd
+from typing import List, Tuple, Union
 
+import pandas as pd
 import torchaudio
 from torch import Tensor
 from torch.utils.data import Dataset
 from torchaudio.datasets.utils import (download_url)
+
+# character we will filter from text sequence, this can be change according
+# to your needs
+FILTER_CHARS = [
+    '"', '%', "'", ',', '-', '.', '/', '\x93', '\x94', '\u200c', '\u200d', '‘',
+    '’', '“', '”', '…', '!', ':'
+]
+
+# english character set to discard
+ENGLISH = {'0', '1', '2', '3', '4', '5', 'B', 'L', 'T', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'l', 'm', 'n', 'o',
+           'p', 'r', 's', 't', 'u', 'v', 'w', 'x', 'z'}
+
+
+def clean(text):
+    """Clean text"""
+    for c in FILTER_CHARS:
+        if c in text:
+            text = text.replace(c, '')
+    return text
+
+
+def contains_english_chars(text):
+    if ENGLISH.intersection(set(text)):
+        return True
+    return False
 
 
 def load_item(file_id: str, path: str, ext_audio: str, df: pd.DataFrame) -> Tuple[Tensor, int, str]:
@@ -19,6 +45,7 @@ def load_item(file_id: str, path: str, ext_audio: str, df: pd.DataFrame) -> Tupl
     waveform, sample_rate = torchaudio.load(file_audio)
 
     return waveform, sample_rate, transcript
+
 
 def unpack_all_in_dir(_dir, extension):
     for item in os.listdir(_dir):  # loop through items in dir
@@ -38,7 +65,10 @@ def unpack_all_in_dir(_dir, extension):
 class SoundDS(Dataset):
     _ext_audio = ".flac"
 
-    def __init__(self, root: Union[str, Path], download: bool = False, df: pd.DataFrame = None):
+    def __init__(self, root: Union[str, Path],
+                 d_keys: List[str] = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'],
+                 download: bool = False,
+                 df: pd.DataFrame = None):
         # Get string representation of 'root' in case Path object is passed
         root = os.fspath(root)
         if not os.path.isdir(root):
@@ -52,37 +82,78 @@ class SoundDS(Dataset):
 
             ext_archive = ".zip"
             base_url = "http://www.openslr.org/resources/53/asr_bengali_"
-            d_keys = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f']
 
             for key in d_keys:
                 url = base_url + key + ext_archive
                 archive = os.path.join(root, FOLDER_IN_ARCHIVE + '_' + key + ext_archive)
                 if not os.path.isfile(archive):
                     download_url(url, root)
-            
+
             self._path = os.path.join(root, FOLDER_IN_ARCHIVE)
-            
+            utt_path = self._path + '/utt_spk_text.tsv'
+            flac_list = sorted(str(p.stem) for p in Path(self._path).glob('*/*/*' + self._ext_audio))
+            flac_set = set(flac_list)
+
             if not os.path.isdir(self._path):
                 unpack_all_in_dir(root, ext_archive)
 
-            df = pd.read_csv(self._path + '/utt_spk_text.tsv', sep='\t', header=None)
-            df.columns = ["id", "hash", "text"]
+            data = []
+            unique_chars = set()
+            max_text_len = 0
+            max_text = ''
+            en_bn_mixed = 0
+
+            with open(utt_path, 'r') as fp:
+                lines = fp.readlines()
+                for line in lines:
+                    line = line.strip(' \n')
+                    line = line.split('\t')
+                    file_name, text = line[0], line[2]
+
+                    if file_name in flac_set:
+                        text = clean(text)
+
+                        # skip english text
+                        if contains_english_chars(text):
+                            en_bn_mixed += 1
+                            continue
+
+                        data.append({'id': file_name, 'text': text})
+
+                        # create unique chars set
+                        for c in text:
+                            unique_chars.add(c)
+
+                        # find max text sequence length, text
+                        text_len = len(text)
+                        if max_text_len < text_len:
+                            max_text_len = text_len
+                            max_text = text
+
+            unique_chars = sorted(unique_chars)
+
+            df = pd.DataFrame.from_dict(data)
             df.sort_values(by=['id'], inplace=True)
             self._df = df
-            
-        
-        self._walker = sorted(str(p.stem) for p in Path(self._path).glob('*/*/*' + self._ext_audio))
 
+            print(f'utt entry       : {len(lines)}')
+            print(f'unique chars    : {len(unique_chars)}')
+            print(f'data            : {len(data)}')
+            print(f"max text length : {max_text_len}")
+            print(f'max text        : {max_text}')
+            print(f'en bn mixed     : {en_bn_mixed}')
+            print(f'flac audio files: {len(flac_list)}')
+            print(f'flac_dic        : {len(flac_set)}')
 
     # ----------------------------
     # Number of items in dataset
     # ----------------------------
     def __len__(self) -> int:
-        return len(self._walker)
+        return len(self._df.index)
 
     # ----------------------------
     # Get i'th item in dataset
     # ----------------------------
     def __getitem__(self, n: int) -> Tuple[Tensor, int, str]:
-        file_id = self._walker[n]
+        file_id = self._df.iloc[n]['id']
         return load_item(file_id, self._path, self._ext_audio, self._df)
